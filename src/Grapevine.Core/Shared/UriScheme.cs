14afd1ec09ad9ea3a9a375dcf6e65a2a5d0ba410
Partiolainen <petersson.kiev@gmail.com>
2018-08-20 17:09:14 +0300

    4.1.1.1 Net Standart 2.0 version

---
+++ src/Grapevine.Core/Shared/UriScheme.cs
@@ -0,0 +1,13 @@
+﻿namespace Grapevine.Core.Shared
+{
+    public enum UriScheme
+    {
+        Http,
+        File,
+        Ftp,
+        Gopher,
+        Https,
+        MailTo,
+        News
+    }
+}
